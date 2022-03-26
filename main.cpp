3cf05c06679915bc30b93a390a79df33b8cae481
Layerex <layerex@dismail.de>
2022-03-26 11:53:18 +0300

    Speed up IO

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, char *argv[])
 {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
     guu::Program program(std::cin);
     program.run(std::cout);
 
